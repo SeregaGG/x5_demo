bb7b413464b89d20d77de9610bb3c4e208563f34
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2022-08-17 14:01:07 +0500

    first commit

---
+++ framework/resources/test_params/email_data.py
@@ -0,0 +1,4 @@
+class BaseEmail:
+    emails = ['kholstinin@lanit.ru']
+    email_subject = "Тема письма. Тестовая рассылка"
+    email_text = "Тестовая рассылка.\n\nТекст письма.\nПросим прощения за доставленные неудобства."
