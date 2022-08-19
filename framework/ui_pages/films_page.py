2f7521f56e443c907a1b87ba142e052e2a712505
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2022-08-20 01:19:56 +0500

    fix xpath selector

--- framework/ui_pages/films_page.py
+++ framework/ui_pages/films_page.py
@@ -12,8 +12,7 @@ class FilmsPage:
             by.xpath('//*[@id="block_left_pad"]/div/div[@class="search_results search_results_last"]'))
 
     def chek_top_five(self, title):
-        top_five = self._films_block.find_elements(By.XPATH, '//*[@id="block_left_pad"]/div/div'
-                                                             '/div[position() >= 2][position() <= 5]/div/p/a')
+        top_five = self._films_block.find_elements(By.XPATH, './/div[position() >= 2][position() <= 5]/div/p/a')
         titles_list = [x.text.lower() for x in top_five]
         attach_allure_screen(self._films_block, attach_name='Блок с фильмами')
         return title in titles_list
