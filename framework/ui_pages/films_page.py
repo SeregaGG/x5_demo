eb39e21de6aa8c19fc6211c64f25fb7df26064f4
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2022-08-20 01:41:13 +0500

    fix search logic

--- framework/ui_pages/films_page.py
+++ framework/ui_pages/films_page.py
@@ -10,8 +10,13 @@ class FilmsPage:
     def __init__(self):
         self._films_block = s(
             by.xpath('//*[@id="block_left_pad"]/div/div[@class="search_results search_results_last"]'))
+        self._most_likely_film = s(by.xpath('//*[@id="block_left_pad"]/div/div[2]/div/div/p/a'))
 
     def chek_top_five(self, title):
+
+        if self._most_likely_film.text == title:
+            return True
+
         top_five = self._films_block.find_elements(By.XPATH, './/div[position() >= 2][position() <= 5]/div/p/a')
         titles_list = [x.text.lower() for x in top_five]
         attach_allure_screen(self._films_block, attach_name='Блок с фильмами')
