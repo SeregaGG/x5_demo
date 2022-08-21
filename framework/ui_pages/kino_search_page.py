91ac545a64cf23cc2c19546c1363986412320eb1
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2022-08-21 19:14:57 +0500

    decompose large method + add allure steps

--- framework/ui_pages/kino_search_page.py
+++ framework/ui_pages/kino_search_page.py
@@ -1,4 +1,5 @@
 import selene.factory
+import allure
 from selene.support import by
 from selene.browser import open_url
 from selene.support.jquery_style_selectors import s
@@ -10,8 +11,8 @@ from selenium.webdriver.common.keys import Keys
 
 
 class SearchPage:
-
     GENRES_LIMIT = 3
+    DEFAULT_VALUE = '-'
 
     def __init__(self):
         self._form_search_main = s(by.xpath('//*[@id="formSearchMain"]'))  # block for screen
@@ -20,18 +21,21 @@ class SearchPage:
         self._genre_selector = s(by.xpath('//*[@id="m_act[genre]"]'))  # select type
         self._search_button = s(by.xpath('//*[@id="formSearchMain"]/input[@type="button"]'))
 
-    def search_film(self, **kwargs):
-        self._title_field.send_keys(kwargs.get('title'))
-        attach_allure_screen(self._form_search_main, attach_name=f'Поиск по названию - {kwargs.get("title")}')
+    @allure.step('Заполнение поля поиска по имени')
+    def fill_title_field(self, title):
+        self._title_field.send_keys(title)
+        attach_allure_screen(self._form_search_main, attach_name=f'Поиск по названию - {title}')
+
+    @allure.step('Выбор страны')
+    def select_country(self, country):
         select_country_obj = Select(self._country_selector)
-        country = kwargs.get('country', '-')
         select_country_obj.select_by_visible_text(country)
-        if not country == '-':
+        if not country == self.DEFAULT_VALUE:
             attach_allure_screen(self._form_search_main, attach_name=f'Поиск по стране - {country}')
 
-        genres = kwargs.get('genre', '-')
-        if not genres == '-':
-
+    @allure.step('Выбор жанров')
+    def select_genres(self, genres):
+        if not genres == self.DEFAULT_VALUE:
             if len(genres) > self.GENRES_LIMIT:
                 genres = genres[0:self.GENRES_LIMIT]
 
@@ -40,9 +44,21 @@ class SearchPage:
                 select_genre_obj.select_by_visible_text(genre)
                 attach_allure_screen(self._form_search_main, attach_name=f'Добавлен жанр - {genre}')
 
+    @allure.step('Заполнение блока')
+    def search_film(self, **kwargs):
+
+        self.fill_title_field(kwargs.get('title'))
+
+        self.select_country(kwargs.get('country', self.DEFAULT_VALUE))
+
+        self.select_genres(kwargs.get('genre', self.DEFAULT_VALUE))
+
         attach_allure_screen(self._form_search_main, attach_name='Готовый блок')
+
         self._search_button.click()
+
         films_page = FilmsPage()
+
         return films_page
 
     def open(self):
